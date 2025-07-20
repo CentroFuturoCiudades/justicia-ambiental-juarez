0319d6b0fb7dff7c05215c80e1803a62e8b88c7a
Luz Hernández <luzpatyhr@gmail.com>
2025-07-20 23:21:56 +0200

    donwload button with placeholder

---
+++ src/utils/downloadFile.tsx
@@ -0,0 +1,6 @@
+export function downloadFileFromUrl(url: string, filename: string) {
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+  link.click();
+}
