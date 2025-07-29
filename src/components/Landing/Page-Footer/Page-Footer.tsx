476a7e3cff276caff95fb252e11951343bb8b658
Luz Hernández <luzpatyhr@gmail.com>
2025-07-29 22:38:54 +0200

    landing design update

--- src/components/Landing/Page-Footer/Page-Footer.tsx
+++ src/components/Landing/Page-Footer/Page-Footer.tsx
@@ -4,13 +4,13 @@ import "./Page-Footer.scss";
 const PageFooter = () => {
   return (
     <footer className="footer-container">
-        {FOOTER_ITEMS.map((item, index) => (
-            <div key={index} className="image-container">
-                <img src={item.imageUrl} alt={item.title} />
-            </div>
-        ))}
+      {FOOTER_ITEMS.map((item, index) => (
+        <div key={index} className="image-container">
+          <img src={item.imageUrl} alt={item.title} />
+        </div>
+      ))}
     </footer>
   );
-}
+};
 
 export default PageFooter;
--- src/components/Landing/Page-Header/PageHeader.tsx
+++ src/components/Landing/Page-Header/PageHeader.tsx
@@ -1,56 +1,40 @@
 import "./PageHeader.scss";
-import { FaLocationDot } from "react-icons/fa6";
 import { LuMailPlus } from "react-icons/lu";
-import { GoDownload } from "react-icons/go";
-import { GoPlus } from "react-icons/go";
+import {GoPlus } from "react-icons/go";
+import { BiGlobe } from "react-icons/bi";
 
 export default function PageHeader() {
     return (
         <header className="page-header">
             <div className="page-header__content">
                 <div className="page-header__logo">
-                    <div className="page-header__logo-title">
-                        <h1 className="page-header__title">
-                            <span className="line start">herramienta</span>
-                            <span className="line center">de evaluación</span>
-                            <span className="line start">ambiental</span>
-                        </h1>
-                    </div>
+                    <p className="page-header__title">
+                        <span className="page-header__titleItalic">visor de <br /></span>
+                        <span className="page-header__titleBold">indicadores ambientales</span>
+                    </p>
                     <div className="page-header__logo-location-icon">
-                        <svg className="page-header__line" viewBox="0 0 100 1" preserveAspectRatio="none" aria-hidden="true">
-                            <line x1="0" y1="0" x2="100" y2="0" stroke="black" strokeWidth="1" />
-                        </svg>
-                        <FaLocationDot />
+                        <div className="page-header__line" />
                     </div>
                 </div>
-            </div>
-            <div className="page-header__speed-dial">
-                <div className="page-header__speed-dial-item">
-                    <button>
-                        <span className="button-text">Send Email</span>
-                        <span className="icon">
-                            <LuMailPlus />
-                        </span>
-                    </button>
-
-                </div>
-                <div className="page-header__speed-dial-item">
-                    <button>
-                        <span className="button-text">Download</span>
-                        <span className="icon">
-                            <GoDownload />
-                        </span>
-                    </button>
-                </div>
-                <div className="page-header__speed-dial-item">
-                    <button>
-
-                        <span className="button-text">Add New</span>
-                        <span className="icon">
-                            <GoPlus />
-                        </span>
-
-                    </button>
+                <div className="page-header__speed-dial">
+                    <div className="page-header__speed-dial-item">
+                        <button type="button" aria-label="Ir al visor" onClick={() => window.location.href = "/visor"}>
+                            <span className="button-text">Ir al visor</span>
+                            <span className="icon"><BiGlobe /></span>
+                        </button>
+                    </div>
+                    <div className="page-header__speed-dial-item">
+                        <button type="button" aria-label="Email Us">
+                            <span className="button-text">Enviar correo </span>
+                            <span className="icon"><LuMailPlus /></span>
+                        </button>
+                    </div>
+                    <div className="page-header__speed-dial-item">
+                        <button type="button" aria-label="Add New">
+                            <span className="button-text">Add New</span>
+                            <span className="icon"><GoPlus /></span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </header>
